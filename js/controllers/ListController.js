90310ade7cde71430328f19cd9890998e2440fac
RohitasBehera <indians4german@gmail.com>
2016-11-16 11:26:10 +0530

    Pre storage

--- js/controllers/ListController.js
+++ js/controllers/ListController.js
@@ -1,27 +1,26 @@
-app.controller('ListController', ['$scope', '$firebaseArray', '$firebaseObject', 'FBURL', function($scope, $firebaseArray, $firebaseObject, FBURL){
+app.controller('ListController', ['$scope', '$firebaseArray','$location', '$firebaseObject', 'FBURL', function($scope, $firebaseArray, $location,$firebaseObject, FBURL){
   
   var products = new Firebase(FBURL);
   $scope.products = $firebaseArray(products);
-  
-  $scope.removeProduct = function(id) {
-    var ref = new Firebase(FBURL + id);
-    var product = $firebaseObject(ref)
-    product.$remove();
-   };
-   
    //detecting input
 	$scope.newValue = function(value) {
-		$scope.value=value;
+		$scope.value=value;		 
 	};
    //adding responses
    
    $scope.addProduct = function() {
 	    $scope.counter = $scope.counter + 1;
+		 if($scope.counter >= $scope.products.length)
+		 {		
+			 alert('Thankyou !!!');
+			 $location.path('/add');			 
+		 };
 		var ref1 = new Firebase("https://test-55ef7.firebaseio.com/Response/");
 		var response = $firebaseArray(ref1);
 		response.$add({
 			QuestionID: $scope.counter,
-			ResponseID: $scope.value
+			ResponseID: $scope.value,
+			User: $scope.username
 		});	
 		// after response nullify $scope.value
 			$scope.value = false;
