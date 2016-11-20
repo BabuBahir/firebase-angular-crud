c1a3c1f8a2b1f9cacc4a561a86c33fa543a32f64
RohitasBehera <indians4german@gmail.com>
2016-11-20 19:42:48 +0530

    progress barr added

--- js/controllers/ListController.js
+++ js/controllers/ListController.js
@@ -2,11 +2,18 @@ app.controller('ListController', ['$scope', '$firebaseArray','$location', '$fire
   
   var products = new Firebase(FBURL);
   $scope.products = $firebaseArray(products);
-  $scope.btn_class = "btn btn-danger";
+  
   //deleting previous response
     var ref = new Firebase("https://test-55ef7.firebaseio.com/Response/");
     var allResponse = $firebaseObject(ref)
     allResponse.$remove(); 
+  
+  //init
+	$scope.init = function() {
+		$scope.btn_class = "btn btn-danger";
+		$scope.counter = 0;
+		$scope.score = 0;
+	}
   
    //detecting input
 	$scope.newValue = function(value) {
@@ -22,13 +29,19 @@ app.controller('ListController', ['$scope', '$firebaseArray','$location', '$fire
    //adding responses   
    $scope.addProduct = function() {
 	    $scope.counter = $scope.counter + 1; 		
+		
+		// increment score if correct answer
+		if($scope.value == $scope.products[$scope.counter-1].answer){
+			$scope.score =$scope.score +1;	
+		}
+		
 		 if($scope.counter >= $scope.products.length)
 		 {						 
 			 // passing name to rootscope
 			 $rootScope.test = $scope.username;
-			 // redirecting
+			 // redirecting if tests finsished
 			 $location.path('/end');			 
-		 };
+		 };				
 		var ref = new Firebase("https://test-55ef7.firebaseio.com/Response/");
 		var response = $firebaseArray(ref);
 		response.$add({
